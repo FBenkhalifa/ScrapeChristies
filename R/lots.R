516e1f3acca782ca4011dbbd24267dccc679a8d0
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2019-12-31 00:03:01 +0100

    Clean fourth way

--- R/lots.R
+++ R/lots.R
@@ -2,20 +2,46 @@
 # Filter level ------------------------------------------------------------
 
 # Get lots names on lots page
-lots <- read_html(URL_FILTERED)
-lots_names <- lots %>% html_nodes(xpath = "//h6/a[@target ='_self']") %>% html_attr("href")
-lots_URLs <- lots %>%
+auction <- read_html(URL_FILTERED)
+auction_names <- auction %>% html_nodes(xpath = "//h6/a[@target ='_self']") %>% html_attr("href")
+auction_URLs <- auction %>%
   html_nodes(xpath = "//h6/a[@target ='_self']") %>%
   html_attr("href") %>%
   paste0(URL, .)
 
 # Lot level ---------------------------------------------------------------
-lots_d <- read_html(lots_URLs[1])
+lots <- read_html(auction_URLs[1])
 # Get summary on lot page
-lots_summary <- lots_d %>% html_nodes("strong , em") %>% html_text
-lot_print <- lots_d %>% html_nodes(xpath = "//a[@target = '_blank' and @class ='print--page']") %>% html_attr("href")
+lots_summary <- lots %>% html_nodes("strong , em") %>% html_text
 
-URL_results <- lots_d %>% html_nodes("#LotListings") %>% html_nodes(xpath = "//a") %>% html_attr("href") %>% .[1]
+URL_print <- lots %>% html_nodes(xpath = "//a[@target = '_blank' and @class ='print--page']") %>% html_attr("href")
+URL_results <- lots %>% html_nodes("#LotListings") %>% html_nodes(xpath = "//a") %>% html_attr("href") %>% .[1]
+
+
+
+# Lots level --------------------------------------------------------------
+
+lots_print <- read_html(URL_print)
+URL_image <-  URL_summary %>% html_nodes("img") %>% html_attr("src")
+
+
+description <- lots_print %>% html_nodes(".lot-info .lot-description") %>% html_text
+period <- lots_print %>% html_nodes(".lot-maker") %>% html_text
+estimate <- lots_print %>% html_nodes(xpath = "//td[@class ='estimate']//span[@class ='lot-description'][1]") %>% html_text
+price <- lots_print %>% html_nodes(xpath = "//td[@class ='estimate']//span[@class ='lot-description'][2]") %>% html_text
+dimensions <- lots_print %>% html_nodes(".medium-dimensions") %>% html_text
+
+lot_table <- tibble(
+  description = description,
+  period = period,
+  estimate = estimate,
+  price = price,
+  dimensions = dimensions,
+  US_prices = US_prices
+)
+
+
+# Results -----------------------------------------------------------------
 
 lots_results <- URL_results %>% read_html
 US_prices <- lots_results %>%
@@ -24,52 +50,24 @@ US_prices <- lots_results %>%
   sub(",", ".", ., fixed = TRUE) %>%
   as.numeric()
 
-# Lots level --------------------------------------------------------------
-
-# Extract lot id to construct lot elements
-read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".filter--lots-wrapper--items") %>% html_attr("id")
-
-# Extract lot name to construct lot elements
-read_html(myclient$getPageSource()[[1]]) %>% html_nodes("#ResultContainer .font_medium") %>% html_text(trim = TRUE)
-
-# Get link to the lot page
-xpath <- "//div[@class ='image-preview-container']/a[@onclick]/@href"
-read_html(myclient$getPageSource()[[1]]) %>%
-  html_nodes(xpath = xpath) %>%
-  html_text(trim = TRUE)
+lots_results <- URL_results %>% read_html
+US_prices <- lots_results %>%
+  html_nodes(xpath = "//span[contains(@id, 'dlResults_lblPrice_')]") %>%
+  html_text() %>%
+  sub(",", ".", ., fixed = TRUE) %>%
+  as.numeric()
 
 
-# Elements level ----------------------------------------------------------
-
-read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".lotDetails")  %>% html_text(trim = TRUE)
-
-
-# Overview level ----------------------------------------------------------
-
-read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".print--page") %>%
-  html_attr("href")
-read_html("https://www.christies.com/lotfinder/print_sale.aspx?saleid=28361&lid=1") %>%
-  html_text
-
-https://www.christies.com/lotfinder/lot/a-pair-of-chinese-red-overlay-yellow-glass-5913075-details.aspx?from=salesummery&intobjectid=5913075&sid=32881f09-dada-4e0f-aad8-e03bc5c6a780
-
-# Get header of filter list -----------------------------------------------
-
-read_html(myclient$getPageSource()[[1]]) %>% html_node(".checkbox") %>% html_text
-read_html(myclient$getPageSource()[[1]]) %>% html_node("#refine-results .ng-scope") %>% html_text
-myclient$getTitle()
-
-
-myclient$navigate("http://www.google.com/ncr")
-webElem <- myclient$findElements(using = "css selector", ".gsfi")
-myclient$click()
+# Images ------------------------------------------------------------------
+URL_image <-  URL_summary %>% html_nodes("img") %>% html_attr("src")
 
 
 
 
-read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".filter--lots-wrapper--items") %>%
-  html_nodes(xpath = "/div/a[@href]")
 
-read_html("https://www.christies.com/lotfinder/print_sale.aspx?saleid=28361&lid=1") %>%
-  html_nodes(".lot-description")
+
+
+
+
+
 
---
+++ R/rests.R
@@ -0,0 +1,107 @@
+# Reste
+
+
+# Chunk 1 -----------------------------------------------------------------
+
+URL_l2 <- read_html(URL_xhr) %>%
+  html_nodes(xpath = "//div[@class='image-preview-container']/a[@href]") %>% html_attr("href")
+
+
+URL_xhr <- paste0("https://www.christies.com/AjaxPages/SaleLanding/DisplayLotList.aspx?intsaleid=", id)
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".filter--lots-wrapper--items") %>%
+  html_nodes(xpath = "/div/a[@href]")
+
+
+# chunk 2 -----------------------------------------------------------------
+
+library(httr)
+library(xml2)
+library(rvest)
+library(RSelenium)
+library(tidyverse)
+library(seleniumPipes)
+
+cursor$highlightElement()
+cursor$clickElement()
+myclient$close()
+rD$server$stop()
+
+# 1 Start connection
+rD <- rsDriver(verbose = FALSE, chromever = "79.0.3945.36" )
+myclient <- rD$client
+
+# 2 Navigate to Christies page
+myclient$navigate(URL_FILTERED)
+
+# 1 Start from the auction side and click on filter
+cursor <- myclient$findElement(using = "xpath", value = FILTER_BUTTON)
+cursor$clickElement()
+
+# 2 Get on category
+cursor <- myclient$findElement(using = "xpath", value = xpath_l1["Category"])
+cursor$highlightElement()
+cursor$clickElement()
+
+
+cursor <- myclient$findElement(using = "xpath", value = xpath_l2["Fine Art"])
+cursor$highlightElement()
+cursor$clickElement()
+
+cursor <- myclient$findElement(using = "xpath", value = "//*[@id=\"refine-results\"]/cc-filters/div[1]/ul/li[2]/cc-multi-select-box/div/div/div[2]/ul/li[5]/label")
+cursor$clickElement(using = "xpath", value = '"//*[@id=\"refine-results\"]/cc-filters/div[1]/ul/li[2]/cc-multi-select-box/div/div/div[2]/ul/li[5]/label')
+
+
+
+# Filter level ------------------------------------------------------------
+
+# Get lots names on lots page
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".image-description--title") %>% html_text(trim = TRUE)
+read_html(URL_FILTERED) %>% html_nodes(".image-description--title") %>% html_text(trim = TRUE)
+
+
+# Lot level ---------------------------------------------------------------
+
+# Get summary on lot page
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes("#rmjs-1") %>% html_text
+
+
+# Lots level --------------------------------------------------------------
+
+# Extract lot id to construct lot elements
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".filter--lots-wrapper--items") %>% html_attr("id")
+
+# Extract lot name to construct lot elements
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes("#ResultContainer .font_medium") %>% html_text(trim = TRUE)
+
+# Get link to the lot page
+xpath <- "//div[@class ='image-preview-container']/a[@onclick]/@href"
+read_html(myclient$getPageSource()[[1]]) %>%
+  html_nodes(xpath = xpath) %>%
+  html_text(trim = TRUE)
+
+
+# Elements level ----------------------------------------------------------
+
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".lotDetails") %>% html_text(trim = TRUE)
+
+
+# Overview level ----------------------------------------------------------
+
+read_html(myclient$getPageSource()[[1]]) %>% html_nodes(".print--page") %>%
+  html_attr("href")
+read_html("https://www.christies.com/lotfinder/print_sale.aspx?saleid=28361&lid=1") %>%
+  html_text
+
+
+
+# Get header of filter list -----------------------------------------------
+
+read_html(myclient$getPageSource()[[1]]) %>% html_node(".checkbox") %>% html_text
+read_html(myclient$getPageSource()[[1]]) %>% html_node("#refine-results .ng-scope") %>% html_text
+myclient$getTitle()
+
+
+myclient$navigate("http://www.google.com/ncr")
+webElem <- myclient$findElements(using = "css selector", ".gsfi")
+myclient$click()
+
