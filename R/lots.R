b088ea8fd8a79711112b7cee24d7b6c483bb5eac
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2019-12-31 00:39:36 +0100

    Start to automate

--- R/lots.R
+++ R/lots.R
@@ -29,24 +29,12 @@ URL_results <- lots %>% html_nodes("#LotListings") %>% html_nodes(xpath = "//a")
 # Lots level --------------------------------------------------------------
 
 lots_print <- read_html(URL_print)
-URL_image <-  URL_summary %>% html_nodes("img") %>% html_attr("src")
-
 
 description <- lots_print %>% html_nodes(".lot-info .lot-description") %>% html_text
 period <- lots_print %>% html_nodes(".lot-maker") %>% html_text
 estimate <- lots_print %>% html_nodes(xpath = "//td[@class ='estimate']//span[@class ='lot-description'][1]") %>% html_text
 price <- lots_print %>% html_nodes(xpath = "//td[@class ='estimate']//span[@class ='lot-description'][2]") %>% html_text
 dimensions <- lots_print %>% html_nodes(".medium-dimensions") %>% html_text
-
-lot_table <- tibble(
-  description = description,
-  period = period,
-  estimate = estimate,
-  price = price,
-  dimensions = dimensions,
-  US_prices = US_prices
-)
-
 
 # Results -----------------------------------------------------------------
 
@@ -57,16 +45,17 @@ US_prices <- lots_results %>%
   sub(",", ".", ., fixed = TRUE) %>%
   as.numeric()
 
-lots_results <- URL_results %>% read_html
-US_prices <- lots_results %>%
-  html_nodes(xpath = "//span[contains(@id, 'dlResults_lblPrice_')]") %>%
-  html_text() %>%
-  sub(",", ".", ., fixed = TRUE) %>%
-  as.numeric()
-
+lot_table <- tibble(
+  description = description,
+  period = period,
+  estimate = estimate,
+  price = price,
+  dimensions = dimensions,
+  US_prices = US_prices
+) %>% rowid_to_column(var = "id")
 
 # Images ------------------------------------------------------------------
-URL_image <-  URL_summary %>% html_nodes("img") %>% html_attr("src")
+URL_image <-  lots_print %>% html_nodes("#lot-list img") %>% html_attr("src")
 URL_image <-  URL_image %>% map_chr(~gsub("\\?.*", "", .))
 
 magick::image_read(URL_image[2])
